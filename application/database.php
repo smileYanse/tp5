a8b324df4d7ae3f6fcb27dad8ebb64d32dedb910
smile <1157615177@qq.com>
2018-04-18 18:52:26 +0800

    演示数据改正

--- application/database.php
+++ application/database.php
@@ -12,21 +12,21 @@
 return [
     // 数据库类型
     'type'            => 'mysql',
-    // 服务器地址
-    /* 'hostname'        => '118.31.44.206',
-      // 数据库名
-      'database'        => 'jisuyue',
-      // 用户名
-      'username'        => 'jisuyue',
-      // 密码
-      'password'        => 'EGGRRWF3W4232',*/
+    // 服务器地址/*
+//   'hostname'        => '118.31.44.206',
+//      // 数据库名
+//      'database'        => 'jisuyue',
+//      // 用户名
+//      'username'        => 'jisuyue',
+//      // 密码
+//      'password'        => 'EGGRRWF3W4232',
     /*  'hostname' => '127.0.0.1',
       // 数据库名
       'database'        => 'tp5',
       // 用户名
       'username'        => 'root',
       // 密码
-      'password'        => '123456',*/
+/*      'password'        => '123456',*/
     'hostname' => '120.55.52.105',
     // 数据库名
     'database' => 'api1101_jisuyue',
